2b3c86f7095996750782f78f88949e5fe24b388e
Danny Tran <dtran@csumb.edu>
2018-10-24 06:55:35 +0000

    modified index page

--- index.php
+++ index.php
@@ -1,15 +1,43 @@
-<html><body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
+<html>
+    <head>
+        
+        <style type="text/css">
+            body {
+                background-color: #000000;
+                color: #00ff00;
+            }
+            
+            a {
+                color: #ffffff;
+            }
+        
+            #labs, #homeworks {
+                float: left;
+                padding-left: 25px;
+            }
+        </style>
+        
+    </head>    
 
-echo "Danny Tran's CST336 Home Page";
 
-?>
+    <body>
+        <header>
+             <h1>Danny Tran's CST336 Home Page</h1>   
+        </header>
 
-<br><br>
-<a href="labs/lab1/index.html">Lab 1 </a>
-
-</body>
+    
+        <div id="content">
+            <div id="labs">
+                <h2>Labs</h2>
+                <a href="labs/lab1/index.html">Lab 1 </a>
+            </div>
+        
+            <div id="homeworks">
+                <h2>Homeworks</h2>
+                <a href="hw/hw1/index.html">Homework 1</a>
+            </div>
+        </div>
+    
+    
+    </body>
 </html>
