c67efc3c7e84a9b0eb5db3eb9191641558c062a2
Danny Tran <dtran@csumb.edu>
2018-11-07 03:54:16 +0000

    modify index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                 <h2>Labs</h2>
                 <a href="labs/lab1/index.html">Lab 1 </a><br>
                 <a href="labs/lab2/index.php"> Lab 2</a><br>
-                <a href="labs/lab3.index.php">Lab 3</a><br>
+                <a href="labs/lab3/index.php">Lab 3</a><br>
             </div>
         
             <div id="homeworks">
