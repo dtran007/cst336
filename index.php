a8e8c42d69f7f9ad488155b59673a538e9dd4086
Danny Tran <dtran@csumb.edu>
2018-11-25 22:14:12 +0000

    update index

--- index.php
+++ index.php
@@ -35,6 +35,7 @@
                 <a href="labs/lab4/index.php">Lab 4</a><br>
                 <a href="labs/lab5/index.php">Lab 5</a><br>
                 <a href="labs/lab6/login.php">Lab 6</a><br>
+                <a href="labs/lab7/index.php">Lab 7</a><br>
             </div>
         
             <div id="homeworks">
