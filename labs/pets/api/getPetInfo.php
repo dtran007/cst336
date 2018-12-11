d6ac532cb5807758c60fd236416e2dfc2e7121a8
Danny Tran <dtran@csumb.edu>
2018-12-11 05:30:45 +0000

    add tweaks

---
+++ labs/pets/api/getPetInfo.php
@@ -0,0 +1,14 @@
+<?php
+//retrieve data from database about a specific pet
+
+include '../dbConnection.php';
+$dbConn = getDatabaseConnection("pets");
+$sql = "SELECT *, YEAR((CURDATE()) - yob age
+        FROM pets
+        WHERE id = :id";
+$stmt = $dbConn->prepare($sql);
+$stmt->execute(array("id"=>$_GET['id']));
+$resultSet = $stmt->fetch(PDO::FETCH_ASSOC);
+echo json_encode($resultSet);
+
+?>
---
+++ labs/pets/index.php
@@ -0,0 +1,18 @@
+<?php
+    include "dbConnection.php";
+    include 'inc/header.php';
+    
+?>
+
+
+    <!--add carousel component -->
+    <br /><br />
+    <a class="btn btn-outline-primary" href="pets.php" role="button">Adopt Now!</a>
+    
+    <br /><br />
+    <hr>
+    
+    
+<?php
+    include 'inc/footer.php';
+?>
